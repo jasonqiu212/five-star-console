f4e8c74a13af4db22b741bf4d7a1491002548ac8
Jason Qiu <jason_qiu@hotmail.com>
2026-08-08 16:30:19 +0800

    feat: add enums for statuses

--- packages/shared-types/src/order/order.enum.ts
+++ packages/shared-types/src/order/order.enum.ts
@@ -11,3 +11,57 @@ export const OrderItemSeatReplacementScopeMeta = createEnumMeta<OrderItemSeatRep
   [OrderItemSeatReplacementScope.WHOLE]: "Whole",
   [OrderItemSeatReplacementScope.PARTIAL]: "Partial",
 });
+
+export enum BatamProductionStatus {
+  WAITING = "Waiting",
+  PRODUCTION = "Production",
+  READY_FOR_SHIPPING = "ReadyForShipping",
+  SHIPPING = "Shipping",
+  DONE = "Done",
+}
+
+export const BatamProductionStatusMeta = createEnumMeta<BatamProductionStatus>({
+  [BatamProductionStatus.WAITING]: "Waiting",
+  [BatamProductionStatus.PRODUCTION]: "Production",
+  [BatamProductionStatus.READY_FOR_SHIPPING]: "Ready for Shipping",
+  [BatamProductionStatus.SHIPPING]: "Shipping",
+  [BatamProductionStatus.DONE]: "Done",
+});
+
+export enum SgProductionStatus {
+  WAITING = "Waiting",
+  PRODUCTION = "Production",
+  DONE = "Done",
+}
+
+export const SgProductionStatusMeta = createEnumMeta<SgProductionStatus>({
+  [SgProductionStatus.WAITING]: "Waiting",
+  [SgProductionStatus.PRODUCTION]: "Production",
+  [SgProductionStatus.DONE]: "Done",
+});
+
+export enum InstallationStatus {
+  WAITING = "Waiting",
+  SCHEDULED_WORKSHOP = "ScheduledWorkshop",
+  SCHEDULED_ON_SITE = "ScheduledOnSite",
+  CAR_RECEIVED = "CarReceived",
+  DISMANTLING = "Dismantling",
+  READY_FOR_WRAPPING = "ReadyForWrapping",
+  WRAPPING = "Wrapping",
+  READY_FOR_REINSTALLATION = "ReadyForReinstallation",
+  REINSTALLATION = "Reinstallation",
+  DONE = "Done",
+}
+
+export const InstallationStatusMeta = createEnumMeta<InstallationStatus>({
+  [InstallationStatus.WAITING]: "Waiting",
+  [InstallationStatus.SCHEDULED_WORKSHOP]: "Scheduled (Workshop)",
+  [InstallationStatus.SCHEDULED_ON_SITE]: "Scheduled (On-site)",
+  [InstallationStatus.CAR_RECEIVED]: "Car Received",
+  [InstallationStatus.DISMANTLING]: "Dismantling",
+  [InstallationStatus.READY_FOR_WRAPPING]: "Ready for Wrapping",
+  [InstallationStatus.WRAPPING]: "Wrapping",
+  [InstallationStatus.READY_FOR_REINSTALLATION]: "Ready for Reinstallation",
+  [InstallationStatus.REINSTALLATION]: "Reinstallation",
+  [InstallationStatus.DONE]: "Done",
+});
